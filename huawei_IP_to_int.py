6d1c7e48e580363dccb5c7c410df213d1a8b4e88
Arcane <zhaoguow@umich.edu>
2020-12-17 20:20:25 -0500

    ip地址和长整数相互转换

--- huawei_IP_to_int.py
+++ huawei_IP_to_int.py
@@ -1,8 +1,10 @@
 while True:
     try:
+        #输入一个ip地址，转换为整数
         line = input().strip().split('.')
         line_bins = ["{:08b}".format(int(item)) for item in line]
         print(int(''.join(line_bins), base=2))
+        #输入一个长整数，转换为ip地址
         line2 = input().strip()
         line2_bins = str("{:032b}".format(int(line2)))
         line2_bins_splited = [str(int(line2_bins[i:i+8], base=2))+'.' for i in range(0, 32, 8)]
